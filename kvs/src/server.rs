5a2758e9e4c9df1a9690e51345620329855b6dde
shayandesh <shafaghi1378@gmail.com>
2021-07-23 23:13:42 +0430

    Making a connection.

--- kvs/src/server.rs
+++ kvs/src/server.rs
@@ -1,6 +1,27 @@
 use tokio::net::TcpListener;
 use std::future::Future;
+use tracing::{debug, error, info, instrument};
+
+use bytes::BytesMut;
+
+use crate::{Connection};
 
 pub async fn run(listener: TcpListener, shutdown: impl Future) {
-    println!("Server Started");
+    tokio::select! {
+        res = listener.accept() => {
+            let socket = match res {
+                Ok((socket, _)) => socket,
+                Err(e) => {
+                    error!(cause = %e, "failed to accept");
+                    return;
+                }
+            };
+            info!("A Connection accepted");
+            let mut connection = Connection::new(socket);
+            connection.read().await;
+        }
+        _ = shutdown => {
+            info!("Shutting down");
+        }
+    }
 }
