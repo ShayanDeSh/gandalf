0c7287396d638cd5921a9322de79cd2b050c43ff
shayandesh <shafaghi1378@gmail.com>
2021-07-25 15:34:19 +0430

    Added listener struct

--- kvs/src/server.rs
+++ kvs/src/server.rs
@@ -1,4 +1,4 @@
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use std::future::Future;
 use tracing::{debug, error, info, instrument};
 
@@ -6,22 +6,52 @@ use bytes::BytesMut;
 
 use crate::{Connection};
 
+pub struct Listener {
+    listener: TcpListener
+}
+
+
 pub async fn run(listener: TcpListener, shutdown: impl Future) {
+    let mut listener = Listener {
+        listener: listener,
+    };
     tokio::select! {
-        res = listener.accept() => {
-            let socket = match res {
-                Ok((socket, _)) => socket,
-                Err(e) => {
-                    error!(cause = %e, "failed to accept");
-                    return;
-                }
-            };
-            info!("A Connection accepted");
-            let mut connection = Connection::new(socket);
-            connection.read().await;
+        res = listener.run() => {
+            if let Err(err) = res {
+                error!(cause = %err, "failed to accept");
+            }
         }
         _ = shutdown => {
             info!("Shutting down");
         }
     }
 }
+
+impl Listener {
+    pub async fn run(&mut self) -> crate::Result<()> {
+        loop {
+            let (socket, addr) = self.listener.accept().await?;
+            info!("A Connection accepted from addr: {:?}", addr);
+
+            let mut connection = Connection::new(socket);
+
+            tokio::spawn(async move {
+                let frame = match connection.read().await {
+                    Ok(Some(frame)) => frame,
+                    Ok(None) => return,
+                    Err(e) => {
+                        error!(cause = ?e, "connection error");
+                        return;
+                    }
+                };
+
+            });
+
+        }
+    }
+}
+
+
+
+
+
