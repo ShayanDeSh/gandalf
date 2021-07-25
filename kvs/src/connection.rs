0f5e10608c569f7714779132f79116b7e77ac212
shayandesh <shafaghi1378@gmail.com>
2021-07-25 10:17:04 +0430

    Check if a frame has been sent

--- kvs/src/connection.rs
+++ kvs/src/connection.rs
@@ -1,9 +1,13 @@
-use bytes::BytesMut;
+use bytes::{Buf, BytesMut};
 
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 
 use tracing::{debug, error, info, instrument};
+
+use std::io::{Cursor};
+
+use crate::frame::{self, Frame};
 
 #[derive(Debug)]
 pub struct Connection {
@@ -19,10 +23,41 @@ impl Connection {
         }
     }
 
-    pub async fn read(&mut self) {
-        self.stream.read_buf(&mut self.buffer).await;
-        info!("Read {:?}", self.buffer)
+    pub async fn read(&mut self) -> crate::Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
     }
+
+    fn parse_frame(&mut self) -> crate::Result<Option<Frame>> {
+        use frame::Error::Incomplete;
+
+
+        let mut cursor = Cursor::new(&self.buffer[..]);
+        match Frame::check(&mut cursor) {
+
+            Ok(_) => {
+                let len = cursor.position() as usize;
+                cursor.set_position(0);
+                
+            }
+            Err(Incomplete) => Ok(None),
+            Err(e) => Err(e.into())
+        }
+
+
+    }
+
 }
 
 
