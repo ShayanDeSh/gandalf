66f86ebbea6a254b38ac652ce6f304932033c427
shayandesh <shafaghi1378@gmail.com>
2021-07-26 20:06:24 +0430

    Almost completed parse module

--- kvs/src/parse.rs
+++ kvs/src/parse.rs
@@ -1,5 +1,7 @@
-use std::{vec, fmt};
+use std::{vec, fmt, str};
 use crate::Frame;
+use bytes::Bytes;
+
 
 #[derive(Debug)]
 pub struct Parse {
@@ -12,14 +14,58 @@ pub enum ParseError {
     Other(crate::Error)
 }
 
-
 impl Parse {
+    pub fn new(frame: Frame) -> Result<Parse, ParseError> {
+        let array = match frame {
+            Frame::Array(array) => array,
+            frame => return Err(format!("Wrong frame format,\
+                    only can accept arrays but recived {:?}", frame).
+                into()),
+        };
+        Ok(Parse {
+            parts: array.into_iter()
+        })
+    }
+
     pub fn next(&mut self) -> Result<Frame, ParseError> {
         self.parts.next().ok_or(ParseError::EndOfStream)
     }
 
-}
+    pub fn next_string(&mut self) -> Result<String, ParseError> {
+        let frame = self.next()?;
+        match frame {
+            Frame::Simple(s) => Ok(s),
+            Frame::Bulk(data) => str::from_utf8(&data[..])
+                .map(|s| s.to_string())
+                .map_err(|_| "could not parse Bulk frame".into()),
+            frame => Err(format!("Expected bulk or array, recived {:?}",frame).
+                into()),
+        }
+    }
 
+    pub fn next_integer(&mut self) -> Result<u64, ParseError> {
+        let frame = self.next()?;
+        match frame {
+            Frame::Integer(num) => Ok(num),
+            frame => Err(format!("protocol error; expected int frame but got {:?}", frame).into()),
+        }
+    }
+
+    pub fn next_bytes(&mut self) -> Result<Bytes, ParseError> {
+        let frame = self.next()?;
+        match frame {
+            Frame::Simple(s) => Ok(Bytes::from(s.into_bytes())),
+            Frame::Bulk(data) => Ok(data),
+            frame => Err(format!(
+                "protocol error; expected simple frame or bulk frame, got {:?}",
+                frame
+            )
+            .into()),
+        }
+
+    }
+
+}
 
 impl From<String> for ParseError {
     fn from(src: String) -> ParseError {
