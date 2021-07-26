e2a2b46d9fc59a2857c77441e27f24c0959be72c
shayandesh <shafaghi1378@gmail.com>
2021-07-27 00:08:26 +0430

    Added set and get commands

--- kvs/src/command.rs
+++ kvs/src/command.rs
@@ -1,19 +1,41 @@
-use crate::{Frame}
+use crate::{Frame, Parse};
+
+use bytes::Bytes;
 
 #[derive(Debug)]
-enum Command {
-    Get()
+pub enum Command {
+    Get(Get),
+    Set(Set)
 }
 
 
 #[derive(Debug)]
 pub struct Get {
-    key: String
+    key: String,
 }
 
+#[derive(Debug)]
+pub struct Set {
+    key: String,
+    value: Bytes,
+}
 
 impl Command {
     pub fn from_frame(frame: Frame) -> crate::Result<Command> {
+        let mut parse = Parse::new(frame)?;
+
+        let cmd_name = parse.next_string()?.to_lowercase();
+
+        let cmd = match &cmd_name[..] {
+            "get" => Command::Get(Get::from_parse(&mut parse)?),
+            "set" => Command::Set(Set::from_parse(&mut parse)?),
+            _ => {
+                return Err("Could not parse the command".into())
+            }
+        };
+
+        parse.finish()?;
+        return Ok(cmd);
     }
 }
 
@@ -25,10 +47,29 @@ impl Get {
         }
     }
 
+    pub fn from_parse(parse: &mut Parse) -> crate::Result<Get> {
+        let key = parse.next_string()?;
+        Ok(Get {
+            key: key
+        })
+    }
+
     pub fn key(&self) -> &str {
         &self.key
     }
 
+}
+
+
+impl Set {
+    pub fn from_parse(parse: &mut Parse) -> crate::Result<Set> {
+        let key = parse.next_string()?;
+        let value = parse.next_bytes()?;
+        Ok(Set {
+            key: key,
+            value: value
+        })
+    }
 }
 
 
