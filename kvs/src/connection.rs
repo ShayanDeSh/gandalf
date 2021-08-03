23a1c227398ab67669ec8e7e54e857e1a3d933d4
shayandesh <shafaghi1378@gmail.com>
2021-08-03 15:43:35 +0430

    Writing to the connection peer

--- kvs/src/connection.rs
+++ kvs/src/connection.rs
@@ -1,11 +1,11 @@
 use bytes::{Buf, BytesMut};
 
 use tokio::net::TcpStream;
-use tokio::io::{AsyncReadExt, BufWriter};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 
 use tracing::{debug};
 
-use std::io::{Cursor};
+use std::io::{self, Cursor};
 
 use crate::frame::{self, Frame};
 
@@ -64,6 +64,67 @@ impl Connection {
 
             Err(e) => Err(e.into()),
         }
+    }
+
+    async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
+        match frame {
+            Frame::Array(val) => {
+                self.stream.write_u8(b'*').await?;
+                self.write_decimal(val.len() as u64).await?;
+                for entity in &*val {
+                    self.write_value(entity).await?;
+                }
+            }
+            _ => self.write_value(&frame).await?
+        }
+        self.stream.flush().await
+    }
+
+    async fn write_value(&mut self, frame: &Frame) -> io::Result<()> {
+        match frame {
+            Frame::Simple(val) => {
+                self.stream.write_u8(b'+').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Error(val) => {
+                self.stream.write_u8(b'-').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Integer(val) => {
+                self.stream.write_u8(b':').await?;
+                self.write_decimal(*val).await?;
+            }
+            Frame::Null => {
+                self.stream.write_all(b"$-1\r\n").await?;
+            }
+            Frame::Bulk(val) => {
+                let len = val.len();
+
+                self.stream.write_u8(b'$').await?;
+                self.write_decimal(len as u64).await?;
+                self.stream.write_all(val).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Array(_val) => unreachable!(),
+        }
+        Ok(())
+    }
+
+    async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+        use std::io::Write;
+
+        let mut buf = [0u8; 20];
+        let mut buf = Cursor::new(&mut buf[..]);
+
+        write!(&mut buf, "{}", val)?;
+
+        let pos = buf.position() as usize;
+        self.stream.write_all(&buf.get_ref()[..pos]).await?;
+        self.stream.write_all(b"\r\n").await?;
+
+        Ok(())
     }
 
 }
