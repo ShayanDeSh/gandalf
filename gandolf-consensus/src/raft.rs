6f37f7254311be100d100c13a03a24adb25f6434
shayandesh <shafaghi1378@gmail.com>
2021-08-10 02:16:08 +0430

    Follower goes for ellection time out.

--- gandolf-consensus/src/raft.rs
+++ gandolf-consensus/src/raft.rs
@@ -1,11 +1,14 @@
 use std::future::Future;
+use rand::{thread_rng, Rng};
+
+use tokio::time::{sleep_until, Duration, Instant};
 
 use tracing::{debug, info, error};
 use tracing::instrument;
 
 use crate::{NodeID};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum State {
     Follower,
     Candidate,
@@ -27,6 +30,7 @@ pub struct Raft {
 #[derive(Debug)]
 struct Follower <'a> {
     raft: &'a mut Raft,
+    election_timeout: u64
 }
 
 
@@ -57,36 +61,64 @@ impl Raft {
     }
 
     pub async fn run(&mut self) -> crate::Result<()> {
-        match self.state {
-            State::Follower => {
-                Follower::new(self).run().await?;
-            },
-            State::Candidate => {
-                debug!("Running at Candidate State");
-            },
-            State::Leader => {
-                debug!("Running at Leader State");
-            },
-            State::NonVoter => {
-                debug!("Running at NonVoter State");
+        loop {
+            match self.state {
+                State::Follower => {
+                    Follower::new(self, 1500).run().await?;
+                },
+                State::Candidate => {
+                    debug!("Running at Candidate State");
+                    return Ok(());
+                },
+                State::Leader => {
+                    debug!("Running at Leader State");
+                    return Ok(());
+                },
+                State::NonVoter => {
+                    debug!("Running at NonVoter State");
+                    return Ok(());
+                }
             }
         }
+        unreachable!();
         Ok(())
+    }
+
+    pub fn set_state(&mut self, state: State) {
+        self.state = state;
     }
 
 }
 
 impl<'a> Follower<'a> {
-    pub fn new(raft: &'a mut Raft) -> Follower {
+    pub fn new(raft: &'a mut Raft, election_timeout: u64) -> Follower {
         Follower {
-            raft: raft
+            raft: raft,
+            election_timeout: election_timeout
         }
     }
 
     #[instrument(level="trace", skip(self))]
     pub async fn run(&mut self) -> crate::Result<()> {
         debug!("Running at Follower State");
+        while self.is_follower() {
+            let time_out = sleep_until(self.generate_timeout());
+
+            tokio::select! {
+                _ = time_out => self.raft.set_state(State::Candidate),
+            }
+        }
+
         Ok(())
     }
 
+    fn is_follower(&self) -> bool {
+        self.raft.state == State::Follower
+    }
+
+    fn generate_timeout(&self) -> Instant {
+        let random = thread_rng().
+            gen_range(self.election_timeout..self.election_timeout * 2);
+        Instant::now() + Duration::from_millis(random)
+    }
 }
