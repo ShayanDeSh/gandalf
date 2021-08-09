c12f6732a63b034f2cc16531215c07db7b6c52f5
shayandesh <shafaghi1378@gmail.com>
2021-08-10 00:52:34 +0430

    Using run function in raft module

--- gandolf-consensus/src/bin/gandolf.rs
+++ gandolf-consensus/src/bin/gandolf.rs
@@ -1,13 +1,14 @@
-use gandolf_consensus::raft::Raft;
+use gandolf_consensus::raft;
 
 use tracing_subscriber;
+
+use tokio::signal;
 
 #[tokio::main]
 pub async fn main() -> Result<(), gandolf_consensus::Error> {
     tracing_subscriber::fmt::try_init()?;
 
-    let mut raft = Raft::new();
-    raft.run().await;
+    raft::run(signal::ctrl_c()).await;
 
     Ok(())
 }
