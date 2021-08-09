ad98a7138ab15c70f374b74a3086e6daef9263d1
shayandesh <shafaghi1378@gmail.com>
2021-08-10 00:52:56 +0430

    Started implementing follower state

--- gandolf-consensus/src/raft.rs
+++ gandolf-consensus/src/raft.rs
@@ -1,4 +1,9 @@
-use tracing::{debug};
+use std::future::Future;
+
+use tracing::{debug, info, error};
+use tracing::instrument;
+
+use crate::{NodeID};
 
 #[derive(Debug)]
 pub enum State {
@@ -10,21 +15,51 @@ pub enum State {
 
 #[derive(Debug)]
 pub struct Raft {
-    state: State
+    id: NodeID,
+    state: State,
+    current_term: u64,
+    commit_index: u64,
+    last_applied: u64,
+    voted_for: Option<NodeID>
+    
 }
 
+#[derive(Debug)]
+struct Follower <'a> {
+    raft: &'a mut Raft,
+}
+
+
+pub async fn run(shutdown: impl Future) {
+    let mut raft = Raft::new();
+    tokio::select! {
+        res = raft.run() => {
+            if let Err(err) = res {
+                error!(cause = %err, "Caused an error: ");
+            }
+        }
+        _ = shutdown => {
+            info!("Shutting down the server");
+        }
+    }
+}
 
 impl Raft {
     pub fn new() -> Raft {
         Raft {
-            state: State::Follower
+            id: NodeID::new_v4(),
+            state: State::Follower,
+            current_term: 0,
+            commit_index: 0,
+            last_applied: 0,
+            voted_for: None
         }
     }
 
-    pub async fn run(&mut self) {
+    pub async fn run(&mut self) -> crate::Result<()> {
         match self.state {
             State::Follower => {
-                debug!("Running at Follower State");
+                Follower::new(self).run().await?;
             },
             State::Candidate => {
                 debug!("Running at Candidate State");
@@ -36,7 +71,22 @@ impl Raft {
                 debug!("Running at NonVoter State");
             }
         }
+        Ok(())
     }
 
 }
 
+impl<'a> Follower<'a> {
+    pub fn new(raft: &'a mut Raft) -> Follower {
+        Follower {
+            raft: raft
+        }
+    }
+
+    #[instrument(level="trace", skip(self))]
+    pub async fn run(&mut self) -> crate::Result<()> {
+        debug!("Running at Follower State");
+        Ok(())
+    }
+
+}
