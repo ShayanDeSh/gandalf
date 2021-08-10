57ddc7b7cf995676aa4e38d5d23de5212364aa6a
shayandesh <shafaghi1378@gmail.com>
2021-08-10 22:46:26 +0430

    Added candidate struct and rpc channels and nodes hashset.

--- gandolf-consensus/src/raft.rs
+++ gandolf-consensus/src/raft.rs
@@ -1,12 +1,14 @@
 use std::future::Future;
+use std::collections::HashSet;
 use rand::{thread_rng, Rng};
 
 use tokio::time::{sleep_until, Duration, Instant};
+use tokio::sync::{mpsc};
 
 use tracing::{debug, info, error};
 use tracing::instrument;
 
-use crate::{NodeID};
+use crate::{NodeID, Node};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum State {
@@ -16,6 +18,9 @@ pub enum State {
     NonVoter
 }
 
+pub enum RaftMessage {
+}
+
 #[derive(Debug)]
 pub struct Raft {
     id: NodeID,
@@ -23,8 +28,9 @@ pub struct Raft {
     current_term: u64,
     commit_index: u64,
     last_applied: u64,
-    voted_for: Option<NodeID>
-    
+    voted_for: Option<NodeID>,
+    all_nodes: HashSet<Node>,
+    rx_rpc: mpsc::UnboundedReceiver<RaftMessage>
 }
 
 #[derive(Debug)]
@@ -33,9 +39,16 @@ struct Follower <'a> {
     election_timeout: u64
 }
 
+#[derive(Debug)]
+struct Candidate <'a> {
+    raft: &'a mut Raft
+}
+
 
 pub async fn run(shutdown: impl Future) {
-    let mut raft = Raft::new();
+    let (_tx_rpc, rx_rpc) = mpsc::unbounded_channel();
+
+    let mut raft = Raft::new(HashSet::new(), rx_rpc);
     tokio::select! {
         res = raft.run() => {
             if let Err(err) = res {
@@ -49,14 +62,17 @@ pub async fn run(shutdown: impl Future) {
 }
 
 impl Raft {
-    pub fn new() -> Raft {
+    pub fn new(nodes: HashSet<Node>,
+        rx_rpc: mpsc::UnboundedReceiver<RaftMessage>) -> Raft {
         Raft {
             id: NodeID::new_v4(),
             state: State::Follower,
             current_term: 0,
             commit_index: 0,
             last_applied: 0,
-            voted_for: None
+            voted_for: None,
+            all_nodes: nodes,
+            rx_rpc: rx_rpc
         }
     }
 
@@ -67,7 +83,7 @@ impl Raft {
                     Follower::new(self, 1500).run().await?;
                 },
                 State::Candidate => {
-                    debug!("Running at Candidate State");
+                    Candidate::new(self).run().await?;
                     return Ok(());
                 },
                 State::Leader => {
@@ -120,5 +136,17 @@ impl<'a> Follower<'a> {
         let random = thread_rng().
             gen_range(self.election_timeout..self.election_timeout * 2);
         Instant::now() + Duration::from_millis(random)
+    }
+}
+
+impl<'a> Candidate<'a> {
+    pub fn new(raft: &'a mut Raft) -> Candidate {
+        Candidate {
+            raft: raft
+        }
+    }
+
+    pub async fn run(&mut self) -> crate::Result<()> {
+        unimplemented!();
     }
 }
