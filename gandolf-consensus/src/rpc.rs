2f855ea3fcbd514ea82fa4e238d0b4c4a483ac3e
shayandesh <shafaghi1378@gmail.com>
2021-08-10 16:16:21 +0430

    RaftRpc function defenitions.

---
+++ gandolf-consensus/src/rpc.rs
@@ -0,0 +1,22 @@
+use tonic::{Request, Response, Status};
+
+use crate::raft_rpc::raft_rpc_server::{RaftRpc, RaftRpcServer};
+use crate::raft_rpc::{AppendEntriesRequest, Entry, AppendEntriesResponse};
+use crate::raft_rpc::{RequestVoteRequest, RequestVoteResponse};
+
+#[derive(Debug)]
+struct RaftRpcService;
+
+#[tonic::async_trait]
+impl RaftRpc for RaftRpcService {
+    async fn append_entries(&self,
+        request: Request<tonic::Streaming<AppendEntriesRequest>>) ->
+        Result<Response<AppendEntriesResponse>, Status> {
+        unimplemented!();
+    }
+
+    async fn request_vote(&self, request: Request<RequestVoteRequest>) -> 
+        Result<Response<RequestVoteResponse>, Status> {
+            unimplemented!();
+    }
+}
