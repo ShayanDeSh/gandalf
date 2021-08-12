2353ad31fd79b42c2ea8aaf82fe7764f3a8f1405
shayandesh <shafaghi1378@gmail.com>
2021-08-12 13:36:09 +0430

    Passing a socket to raft::run

--- gandolf-consensus/src/bin/gandolf.rs
+++ gandolf-consensus/src/bin/gandolf.rs
@@ -4,11 +4,14 @@ use tracing_subscriber;
 
 use tokio::signal;
 
+
 #[tokio::main]
 pub async fn main() -> Result<(), gandolf_consensus::Error> {
     tracing_subscriber::fmt::try_init()?;
 
-    raft::run(signal::ctrl_c()).await;
+    let addr = "127.0.0.1:10000".parse().unwrap();
+
+    raft::run(signal::ctrl_c(), addr).await;
 
     Ok(())
 }
