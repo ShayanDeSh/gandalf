7dd886a80de40327f85a1bdc4c35a9e7fde16569
shayandesh <shafaghi1378@gmail.com>
2021-08-14 23:46:41 +0430

    Added gandolf cli.

--- gandolf-consensus/src/bin/gandolf.rs
+++ gandolf-consensus/src/bin/gandolf.rs
@@ -1,19 +1,48 @@
-use gandolf_consensus::raft;
+use gandolf_consensus::{raft, ConfigMap};
+use gandolf_consensus::{DEFAULT_PORT, HEARTBEAT, TIMEOUT};
+
 
 use tracing_subscriber;
-
 use tokio::signal;
+
+use structopt::StructOpt;
 
 
 #[tokio::main]
 pub async fn main() -> Result<(), gandolf_consensus::Error> {
     tracing_subscriber::fmt::try_init()?;
 
-    let addr = "127.0.0.1:10000".parse().unwrap();
+//    let addr = "127.0.0.1:10000".parse().unwrap();
 
-    raft::run(signal::ctrl_c(), addr).await;
+    let cli = Cli::from_args();
+
+
+    let nodes = cli.nodes.ok_or("You must pass list of nodes")?;
+
+    let config = ConfigMap::new(cli.host, cli.port, nodes, cli.heartbeat,
+        cli.timeout);
+
+    raft::run(signal::ctrl_c(), config).await?;
 
     Ok(())
 }
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gandolf", version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"), about = "gandolf consensus system")]
+struct Cli {
+    #[structopt(name = "port", short = "-p", long = "--port", default_value = DEFAULT_PORT)]
+    port: u16,
 
+    #[structopt(name = "host", short = "-h" ,long = "host", default_value = "127.0.0.1")]
+    host: String,
+
+    #[structopt(name = "nodes", long = "--node")]
+    nodes: Option<Vec<String>>,
+
+    #[structopt(name = "heartbeat", long = "--heart", default_value = HEARTBEAT)]
+    heartbeat: u64,
+
+    #[structopt(name = "timeout", long = "--timeout", default_value = TIMEOUT)]
+    timeout: u64,
+}
