c2ebe8cba76de7215e7f37cc3e230fefedf16a8f
shayandesh <shafaghi1378@gmail.com>
2021-08-26 21:28:50 +0430

    Differentiating between write and read requests.

--- gandolf-consensus/src/client/kvs/parser.rs
+++ gandolf-consensus/src/client/kvs/parser.rs
@@ -1,9 +1,10 @@
 use bytes::{Buf, BytesMut};
 use std::io::Cursor;
 
-pub use gandolf_kvs::frame::{self, Frame};
+use gandolf_kvs::frame::{self, Frame};
+use gandolf_kvs::Command;
 
-use crate::parser::Parser;
+use crate::parser::{Parser, Kind};
 
 #[derive(Clone, Debug)]
 pub struct KvsParser;
@@ -11,7 +12,7 @@ pub struct KvsParser;
 impl crate::ClientData for Frame {}
 
 impl Parser<Frame> for KvsParser {
-    fn parse(&self, buffer: &mut BytesMut) -> crate::Result<Option<Frame>> {
+    fn parse(&self, buffer: &mut BytesMut) -> crate::Result<Option<Kind<Frame>>> {
         let mut cursor = Cursor::new(&buffer[..]);
         match Frame::check(&mut cursor) {
             Ok(_) => {
@@ -22,7 +23,11 @@ impl Parser<Frame> for KvsParser {
                 
                 buffer.advance(len);
 
-                Ok(Some(frame))
+                match Command::from_frame(frame.clone())? {
+                    Command::Get(_) => return Ok(Some(Kind::Read(frame))),
+                    Command::Set(_) => return Ok(Some(Kind::Write(frame)))
+                }
+
             }
 
             Err(frame::Error::Incomplete) => Ok(None),
--- gandolf-consensus/src/parser.rs
+++ gandolf-consensus/src/parser.rs
@@ -2,6 +2,11 @@ use bytes::BytesMut;
 
 use crate::ClientData;
 
+pub enum Kind<T: ClientData> {
+    Read(T),
+    Write(T)
+}
+
 pub trait Parser<T: ClientData>: Send + Sync + Clone + 'static {
-    fn parse(&self, buffer: &mut BytesMut) -> crate::Result<Option<T>>;
+    fn parse(&self, buffer: &mut BytesMut) -> crate::Result<Option<Kind<T>>>;
 }
