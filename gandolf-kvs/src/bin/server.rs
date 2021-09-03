9bcc0f7197acd279ca84cd574a80a31d5c8d7a1f
shayandesh <shafaghi1378@gmail.com>
2021-09-03 23:41:12 +0430

    Added host to command line and set up env

--- gandolf-kvs/src/bin/server.rs
+++ gandolf-kvs/src/bin/server.rs
@@ -4,6 +4,8 @@ use structopt::StructOpt;
 use tokio::net::TcpListener;
 use tokio::signal;
 
+use std::env;
+
 use tracing::info;
 use tracing_subscriber;
 
@@ -12,10 +14,13 @@ pub async fn main() -> Result<(), gandolf_kvs::Error> {
     tracing_subscriber::fmt::try_init()?;
 
     let cli = Cli::from_args();
-    let port: &str = cli.port.as_deref().unwrap_or(gandolf_kvs::DEFAULT_PORT);
-    let listener = TcpListener::bind(&format!("127.0.0.1:{}", port)).await?;
 
-    info!("Listening to {:?}", port);
+    let port = env::var("GANDOLF_KVS_PORT").unwrap_or(cli.port);
+    let host = env::var("GANDOLF_KVS_HOST").unwrap_or(cli.host);
+
+    let listener = TcpListener::bind(&format!("{}:{}", host, port)).await?;
+
+    info!("Listening to {}:{}", host, port);
 
     server::run(listener, signal::ctrl_c()).await;
     Ok(())
@@ -26,8 +31,11 @@ pub async fn main() -> Result<(), gandolf_kvs::Error> {
 #[structopt(name = "gandolf-kvs-server", version = env!("CARGO_PKG_VERSION"),
     author = env!("CARGO_PKG_AUTHORS"), about = "gandolf's key value store")]
 struct Cli {
-    #[structopt(name = "port", short = "-p", long = "--port")]
-    port: Option<String>
+    #[structopt(name = "port", short = "-p", long = "--port", default_value = "127.0.0.1")]
+    port: String,
+
+    #[structopt(name = "host", long = "--host", default_value = gandolf_kvs::DEFAULT_PORT)]
+    host: String,
 }
 
 
