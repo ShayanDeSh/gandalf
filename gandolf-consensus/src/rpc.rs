ebced6ac373c3d66f458e0e2377dc218bec9a38b
shayandesh <shafaghi1378@gmail.com>
2021-09-11 00:58:47 +0430

    Implemented forward rpc client and server

--- gandolf-consensus/src/rpc.rs
+++ gandolf-consensus/src/rpc.rs
@@ -5,12 +5,13 @@ use crate::raft_rpc::raft_rpc_client::RaftRpcClient;
 
 use crate::raft_rpc::{AppendEntriesRequest, AppendEntriesResponse};
 use crate::raft_rpc::{RequestVoteRequest, RequestVoteResponse};
+use crate::raft_rpc::{ForwardEntryRequest, ForwardEntryResponse};
 
 use crate::{Node, RaftMessage, ClientData};
 
 use tokio::sync::{mpsc, oneshot};
 
-use tracing::info;
+use tracing::{info, error};
 
 #[derive(Debug)]
 pub struct RaftRpcService<T: ClientData> {
@@ -57,7 +58,7 @@ impl<T: ClientData> RaftRpc for RaftRpcService<T> {
         let body = request.into_inner();
         info!("{:?} Asked for vote", &body.candidate_id);
         let resp = self.tx_rpc.send(RaftMessage::VoteMsg{
-            body: body,
+            body,
             tx
         }); 
         if let Err(err) = resp {
@@ -77,6 +78,50 @@ impl<T: ClientData> RaftRpc for RaftRpcService<T> {
             _ => {return Err(Status::unknown("Unkown response recived"));}
         }
     }
+
+    async fn forward_entry(&self, request: Request<ForwardEntryRequest>) ->
+        Result<Response<ForwardEntryResponse>, Status> {
+        let (tx, rx) = oneshot::channel();
+        let req = request.into_inner();
+
+        let entity: T = match serde_json::from_str(&req.payload) {
+            Ok(entity) => entity,
+            Err(err) => {
+                error!(cause = %err, "Caused an error: ");
+                return Err(Status::invalid_argument("Could not parse the entity")); 
+            }
+        };
+
+        let msg = if req.iswrite {
+            RaftMessage::ClientWriteMsg {
+                body: entity,
+                tx
+            }
+        } else {
+            RaftMessage::ClientReadMsg {
+                body: entity,
+                tx
+            }
+        };
+        let resp = self.tx_rpc.send(msg);
+
+        if let Err(err) = resp {
+            return Err(Status::internal(err.to_string()));
+        }
+        let resp = match rx.await {
+            Ok(msg) => msg,
+            Err(err) => return Err(Status::internal(err.to_string()))
+        };
+        match resp {
+            RaftMessage::ClientResp{body} => {
+                let payload = serde_json::to_string(&body).unwrap();
+                return Ok(Response::new(ForwardEntryResponse { payload }));
+            },
+            _ => {return Err(Status::unknown("Unkown response recived"));}
+        }
+
+    }
+
 }
 
 pub async fn ask_for_vote(node: &Node, request: RequestVoteRequest) 
@@ -95,5 +140,14 @@ pub async fn append_entries(node: &Node, request: AppendEntriesRequest)
     let addr = format!("http://{}:{}", node.ip, node.port);
     let mut client = RaftRpcClient::connect(addr).await?;
     let response = client.append_entries(request).await?;
+    Ok(response.into_inner())
+}
+
+pub async fn forward(node: &Node, request: ForwardEntryRequest) 
+    -> crate::Result<ForwardEntryResponse> {
+    let addr = format!("http://{}:{}", node.ip, node.port);
+    info!("Forwarding entry to {}:{}", node.ip, node.port);
+    let mut client = RaftRpcClient::connect(addr).await?;
+    let response = client.forward_entry(request).await?;
     Ok(response.into_inner())
 }
