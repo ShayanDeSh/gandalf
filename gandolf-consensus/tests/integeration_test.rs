36bf124641c3a8ae1fd814eaf63d08c4ea64358f
shayandesh <shafaghi1378@gmail.com>
2021-10-02 00:06:19 +0330

    Direct and Indirect client read tests

--- gandolf-consensus/tests/integeration_test.rs
+++ gandolf-consensus/tests/integeration_test.rs
@@ -66,6 +66,19 @@ async fn client_write_requset(count: u32, addr: String, sleep_duration: Duration
     Ok(())
 }
 
+async fn client_read_requset(count: u32, addr: String, sleep_duration: Duration) -> gandolf_consensus::Result<()> {
+    sleep(sleep_duration).await;
+    let mut con = client::connect(addr).await?;
+    for i in 0..count {
+        let key = format!("foo{}", i);
+        if let None = con.get(&key).await? {
+            assert!(false);
+        }
+    }
+    sleep(Duration::from_secs(2)).await;
+    Ok(())
+}
+
 #[tokio::test(flavor = "multi_thread", worker_threads = 5)]
 async fn test_cluster_bootstrap() -> gandolf_consensus::Result<()>{
     let cluster = kvs_cluster_of_nth(5).await?;
@@ -309,6 +322,164 @@ async fn test_indirect_write() -> gandolf_consensus::Result<()> {
     let _: Vec<_> = cluster.
         iter()
         .map(|c| assert!(c.0.borrow().get_commit_index() == 10)).collect();
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 5)]
+async fn test_direct_read() -> gandolf_consensus::Result<()> {
+    let cluster = kvs_cluster_of_nth(5).await?;
+
+    let mut node1 = cluster.get(0).unwrap().0.borrow_mut();
+    let mut node2 = cluster.get(1).unwrap().0.borrow_mut();
+    let mut node3 = cluster.get(2).unwrap().0.borrow_mut();
+    let mut node4 = cluster.get(3).unwrap().0.borrow_mut();
+    let mut node5 = cluster.get(4).unwrap().0.borrow_mut();
+
+    node1.current_term = 1;
+    node1.set_state(State::Leader);
+
+    node2.current_term = 1;
+    node2.current_leader = Some(node1.id.clone());
+
+    node3.current_term = 1;
+    node3.current_leader = Some(node1.id.clone());
+
+    node4.current_term = 1;
+    node4.current_leader = Some(node1.id.clone());
+
+    node5.current_term = 1;
+    node5.current_leader = Some(node1.id.clone());
+    
+    let connection_addr = format!("127.0.0.1:{}", 9876).to_string();
+
+    tokio::select! {
+        _ = node1.run() => {
+            assert!(false);
+        },
+        _ = node2.run()  => {
+            assert!(false);
+        },
+        _ = node3.run()  => {
+            assert!(false);
+        },
+        _ = node4.run()  => {
+            assert!(false);
+        },
+        _ = node5.run()  => {
+            assert!(false);
+        },
+        res = client_write_requset(10, connection_addr.clone(), Duration::from_secs(0)) => {
+            res?
+        }
+    }
+
+    tokio::select! {
+        _ = node1.run() => {
+            assert!(false);
+        },
+        _ = node2.run()  => {
+            assert!(false);
+        },
+        _ = node3.run()  => {
+            assert!(false);
+        },
+        _ = node4.run()  => {
+            assert!(false);
+        },
+        _ = node5.run()  => {
+            assert!(false);
+        },
+        res = client_read_requset(10, connection_addr, Duration::from_secs(0)) => {
+            res?
+        }
+    }
+
+    drop(node1);
+    drop(node2);
+    drop(node3);
+    drop(node4);
+    drop(node5);
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 5)]
+async fn test_indirect_read() -> gandolf_consensus::Result<()> {
+    let cluster = kvs_cluster_of_nth(5).await?;
+
+    let mut node1 = cluster.get(0).unwrap().0.borrow_mut();
+    let mut node2 = cluster.get(1).unwrap().0.borrow_mut();
+    let mut node3 = cluster.get(2).unwrap().0.borrow_mut();
+    let mut node4 = cluster.get(3).unwrap().0.borrow_mut();
+    let mut node5 = cluster.get(4).unwrap().0.borrow_mut();
+
+    node1.current_term = 1;
+    node1.set_state(State::Leader);
+
+    node2.current_term = 1;
+    node2.current_leader = Some(node1.id.clone());
+
+    node3.current_term = 1;
+    node3.current_leader = Some(node1.id.clone());
+
+    node4.current_term = 1;
+    node4.current_leader = Some(node1.id.clone());
+
+    node5.current_term = 1;
+    node5.current_leader = Some(node1.id.clone());
+    
+    let connection_addr = format!("127.0.0.1:{}", 9876).to_string();
+
+    tokio::select! {
+        _ = node1.run() => {
+            assert!(false);
+        },
+        _ = node2.run()  => {
+            assert!(false);
+        },
+        _ = node3.run()  => {
+            assert!(false);
+        },
+        _ = node4.run()  => {
+            assert!(false);
+        },
+        _ = node5.run()  => {
+            assert!(false);
+        },
+        res = client_write_requset(10, connection_addr, Duration::from_secs(0)) => {
+            res?
+        }
+    }
+
+    let connection_addr = format!("127.0.0.1:{}", 9877).to_string();
+
+    tokio::select! {
+        _ = node1.run() => {
+            assert!(false);
+        },
+        _ = node2.run()  => {
+            assert!(false);
+        },
+        _ = node3.run()  => {
+            assert!(false);
+        },
+        _ = node4.run()  => {
+            assert!(false);
+        },
+        _ = node5.run()  => {
+            assert!(false);
+        },
+        res = client_read_requset(10, connection_addr, Duration::from_secs(0)) => {
+            res?
+        }
+    }
+
+    drop(node1);
+    drop(node2);
+    drop(node3);
+    drop(node4);
+    drop(node5);
 
     Ok(())
 }
